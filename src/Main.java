2f095faa9b58ec72f3e8aa9ee44ebc534a9af33c
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-10-02 12:33:45 +0200

    number in java

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,52 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Hello and welcome!");
+      System.out.println("the program is starting");
+
+      int amount = 47;
+
+      int cost = -56;
+
+      long amountLong = 9223;
+
+      System.out.println("cost " + amount);
+      System.out.println("cost " + cost);
+      System.out.println("cost " + amountLong);
+
+      float littleDecimalNumber = 4.5f;
+      double bigDecimalNumber = 4.5;
+
+      boolean myBoolean= false; // o true
+
+      char myChar = 'l';
+
+      //Basic operation
+
+      int a = 5;
+      int b = 3;
+      int c = a+b;
 
 
-        for (int i = 0; i <= 5; i++) {
+      System.out.println("the integet A is " + a);
+      System.out.println("the integet A is " + b);
+
+      System.out.println("the sum is "+ c);
+
+      int d = a-b;
+      System.out.println("the difference is "+ d);
 
 
-            System.out.println("i = " + i);
+      int e = a*b;
+      System.out.println("the difference is "+ e);
 
 
-        }
+      double f = (float)a / (float)b;
+      System.out.println("the difference is "+ f);
+
+
+
+      System.out.println("the program is ending");
+
+
     }
 }
